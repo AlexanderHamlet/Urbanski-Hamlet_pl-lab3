d51efb17b57e1d4fa0ba5b9264b44c11019b667e
AlexanderHamlet <alexander.m.hamlet@gmail.com>
2018-02-20 19:24:59 -0700

    Implemented most of substitute. Missing cases for Call() and Function().

--- src/main/scala/jsy/student/Lab3.scala
+++ src/main/scala/jsy/student/Lab3.scala
@@ -209,14 +209,14 @@ object Lab3 extends JsyApplication with Lab3Like {
     e match {
       case N(_) | B(_) | Undefined | S(_) => e
       case Print(e1) => Print(substitute(e1, v, x))
-      case Unary(uop, e1) => ???
-      case Binary(bop, e1, e2) => ???
-      case If(e1, e2, e3) => ???
+      case Unary(uop, e1) => Unary(uop, substitute(e1, v, x))
+      case Binary(bop, e1, e2) => Binary(bop, substitute(e1, v, x), substitute(e2, v, x))
+      case If(e1, e2, e3) => If(substitute(e1, v, x), substitute(e2, v, x), substitute(e3, v, x))
       case Call(e1, e2) => ???
-      case Var(y) => ???
+      case Var(y) => if(y == x) e else Var(y)
       case Function(None, y, e1) => ???
       case Function(Some(y1), y2, e1) => ???
-      case ConstDecl(y, e1, e2) => ???
+      case ConstDecl(y, e1, e2) => if(y == x) ConstDecl(y, substitute(e1, v, x), e2) else ConstDecl(y, substitute(e1, v, x), substitute(e2, v, x))
     }
   }
 
